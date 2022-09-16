efda5c9c29350609b47971f96a09c7779d181c2e
syl051088 <syl051088@gmail.com>
2022-09-16 16:37:26 -0400

    update lab 2

--- Labs/lab-2-balancing-parentheses-master/starter-files/main.cpp
+++ Labs/lab-2-balancing-parentheses-master/starter-files/main.cpp
@@ -1,13 +1,53 @@
-/**
+/**1CAEF3A0FEDD0DEC26BA9808C69D4D22A9962768
  * hello.code
  *
  * Says hello to the world.
  */
 
-
+#include <deque>
 #include <iostream>
 using namespace std;
+bool checkBalance() {
+    deque<char> balanceDec;
+    char c;
 
-int main(int argc, const char* argv[]) {
-    cout << "Hello world!" << endl;
+    while (cin >> c) {
+        if (c == '(' || c == '{' || c == '[') {
+            balanceDec.push_back(c);
+        } else if (c == ')' || c == '}' || c == ']') {
+            if (balanceDec.empty()) {
+                return false;
+            } // if empty
+            if (c == ')') {
+                if (balanceDec.back() == '(') {
+                    balanceDec.pop_back();
+                } else {
+                    return false;
+                } 
+            } else if (c == '}') {
+                if (balanceDec.back() == '{') {
+                    balanceDec.pop_back();
+                } else {
+                    return false;
+                } 
+            } else {
+                if (balanceDec.back() == '[') {
+                    balanceDec.pop_back();
+                } else {
+                    return false;
+                } 
+            }
+        }
+    }
+    return balanceDec.empty();
+}
+
+int main() {
+    if (checkBalance()) {
+        cout << "Balanced\n";
+    } else {
+        cout << "Not balanced\n";
+    }
+
+    return 0;
 }
